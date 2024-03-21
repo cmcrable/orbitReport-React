a384f86ea0522e7e360fa50b90633bccf9e7017e
Curtiss <cmcrable@gmail.com>
2024-03-20 19:19:36 -0500

    Crable Graded Assignment 6 - Oribt Report - CP

---
+++ src/components/Banner.jsx
@@ -0,0 +1,14 @@
+import styles from "./styling.css";
+
+function Banner() {
+    return (
+        <>
+        <header>
+            <h1>Orbit Report</h1>
+        </header>
+        <p>Click on the buttons to see the satellites in that orbit type</p>
+        </>
+    );
+}
+
+export default Banner
--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,10 +1,19 @@
+import styles from "./styling.css";
+import satData from "./satData";
+
 const Buttons = ({ filterByType, setSat, displaySats }) => {
+  const satButton = displaySats.map((sat, id) => {
   return (
-    <div>
-      <button>Placeholder Button</button>
-      <button>All Orbits</button>
-    </div>
+      <button onClick={() => filterByType(sat)} key={id}>
+        {sat} Orbit
+      </button>
   );
-};
+})
+return (
+  <div className="flex-container">
+    {satButton}
+    <button onClick={() => setSat(satData)}>All Orbits</button>
+  </div>
+)};
 
 export default Buttons;
